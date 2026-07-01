5f5602e3ff7737de3830fd98745769efc37ee013
maxms <mamosa235@gmail.com>
2026-07-01 21:43:45 +0200

    Nova actualització amb logo

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.maxms.app',
-  appName: 'redorblack',
+  appName: 'RTB',
   webDir: 'out'
 };
 
